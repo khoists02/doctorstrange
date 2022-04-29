94faeb2095925c89fbaf628c80d94dbf7dfc2084
khoi.le <khoi.le@advahealthsolutions.com>
2022-04-29 22:26:47 +0700

    up

--- assets/main.js
+++ assets/main.js
@@ -1,35 +1,15 @@
-jQuery(document).ready(function () {
-  jQuery('.partner-logo-slider').slick({
-    slidesToShow: 7,
-    slidesToScroll: 1,
-    speed: 4500,
-    autoplaySpeed: 0,
-    autoplay: true,
-    arrows: false,
-    cssEase: 'linear',
-    centerMode: false,
-    centerPadding: '5px',
-    responsive: [
-      {
-        breakpoint: 480,
-        settings: {
-          speed: 4500,
-          autoplaySpeed: 0,
-          autoplay: true,
-          arrows: false,
-          cssEase: 'linear',
-          centerMode: true,
-          centerPadding: '5px',
-          slidesToShow: 3
-        }
-      }
-    ]
-  });
-
-  console.log("init");
-  AOS.init({
-    offset: 400, // offset (in px) from the original trigger point
-    delay: 0, // values from 0 to 3000, with step 50ms
-    duration: 1000 // values from 0 to 3000, with step 50ms
-  });
-});
+function CryptoAddressfunction() {
+  var buttonCopied = document.getElementById("buttonCopied");
+  buttonCopied.setAttribute("title", "");
+  var copyText = document.getElementById("CryptoAddress");
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+   /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+  // if (buttonCopied) {
+  //   buttonCopied.setAttribute("title", `Copied`);
+  // }
+   /* Alert the copied text */
+   alert("Copied the text: " + copyText.value);
+}
